1d87b8a044fe4ca0e4ba45cf1eaf94646213198e
Asma Kolaib <as.kolaib@gmail.com>
2020-08-14 11:06:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,31 +1,24 @@
 // // Add your code here
 
-// let nameUser = document.getElementById("name").value;
-// let email = document.getElementById("email").value;
-function submitData(name , email){
-   let formData = {
-  Name: nameUser,
-  Age: email
-};
- 
-let configObj = {
-  method :"POST" ,
-  headers: {
-    "Content-Type": "application/json",
-    "Accept": "application/json"
-  },
-  body: JSON.stringify(formData)
-};
- 
-fetch("http://localhost:3000/users", configObj)
-  .then(function(response) {
-    return response.json();
-  })
-  .then(function(object) {
-    console.log(object);
-  })
-  .catch(function(error) {
-    alert("Bad things! Ragnarők!");
-    console.log(error.message);
-  });
+function submitData( name, email ) {
+  return fetch( 'http://localhost:3000/users', {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      },
+      body: JSON.stringify( {
+        name,
+        email
+      } )
+    } )
+    .then( function ( response ) {
+      return response.json()
+    } )
+    .then( function ( object ) {
+      document.body.innerHTML = object[ "id" ]
+    } )
+    .catch( function ( error ) {
+      document.body.innerHTML = error.message
+    } )
 }
