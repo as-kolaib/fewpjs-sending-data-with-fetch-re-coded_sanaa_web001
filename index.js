ff3471fdcc9e4e31ff724cefc5d986c3e7c0b60b
Asma Kolaib <as.kolaib@gmail.com>
2020-08-14 11:04:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Add your code here
+// // Add your code here
+
+// let nameUser = document.getElementById("name").value;
+// let email = document.getElementById("email").value;
+function submitData(name , age){
+   let formData = {
+  Name: nameUser,
+  Age: age
+};
+ 
+let configObj = {
+  method :"POST" ,
+  headers: {
+    "Content-Type": "application/json",
+    "Accept": "application/json"
+  },
+  body: JSON.stringify(formData)
+};
+ 
+fetch("http://localhost:3000/users", configObj)
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(object) {
+    console.log(object);
+  })
+  .catch(function(error) {
+    alert("Bad things! Ragnarők!");
+    console.log(error.message);
+  });
+}
